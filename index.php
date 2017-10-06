f5c06025cd55b9d527eace701b6521d6509e7f6b
Rita <rita.perova@gmail.com>
2017-10-06 15:12:40 +0300

    10/6/2017

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>3D Flowers</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <div class="container">
+		<header class="main-header clearfix">
+			<img class="logo" src="images/logo.svg">
+			<h1 class="name">Flowers<span> Gallery</span></h1>
+		</header>
+
+		<div class="content clearfix">
+
+			<div class="cube-container">
+				<div class="photo-cube">
+
+					<img class="front" src="images/1.jpg" alt="flower">
+					<div class="back photo-desc">
+					  <h3>Flower</h3>
+					  <p>Aenean lacinia bibendum nulla sed consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+						<a href="#" class="button">download</a>
+					</div>
+					<img class="left" src="images/4.jpg" alt="flower">
+					<img class="right" src="images/00.jpg" alt="flower">
+
+				</div>
+			</div>	
+
+			<div class="cube-container">
+				<div class="photo-cube">
+
+					<img class="front" src="images/2.jpg" alt="flower">
+					<div class="back photo-desc">
+					  <h3>Flower</h3>
+					  <p>Aenean lacinia bibendum nulla sed consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+						<a href="#" class="button">download</a>
+					</div>
+					<img class="left" src="images/8.jpg" alt="">
+					<img class="right" src="images/000.jpg" alt="">
+
+				</div>
+			</div>	
+
+			<div class="cube-container">
+				<div class="photo-cube">
+
+					<img class="front" src="images/3.jpg" alt="">
+					<div class="back photo-desc">
+					  <h3>Flower</h3>
+					  <p>Aenean lacinia bibendum nulla sed consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+						<a href="#" class="button">download</a>
+					</div>
+					<img class="left" src="images/5.jpg" alt="">
+					<img class="right" src="images/7.jpg" alt="">
+
+				</div>
+			</div>	
+
+		</div>
+	</div>
+  
+  
+</body>
+</html>
